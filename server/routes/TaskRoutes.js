b52d80b7d28be59c0f1df03ef0bd516e4f9b62f4
Ramosh99 <einfetch@gmail.com>
2024-12-21 01:03:20 +0530

    feat: implement server-side task management with RESTful API and Docker support

--- server/routes/TaskRoutes.js
+++ server/routes/TaskRoutes.js
@@ -1,96 +1,11 @@
 import express from 'express'
-import Task from '../models/TaskModel.js'
+import { TaskController } from '../controllers/TaskController.js'
 
 const router = express.Router()
 
-router.post('/add-task', async(req,res) => {
-    console.log('req.body', req.body)
-    const task = new Task(req.body.taskData)
-    console.log(task)
-    try{
-        await task.save()
-        res.status(201).json({
-            status: 'Success',
-            data : {
-                task : task
-            }
-        })
-    }catch(err){
-        res.status(500).json({
-            status: 'Failed',
-            message : err
-        })
-    }
-})
-
-router.get('/get-tasks', async(req,res) => {
-    try{
-        const userId = req.query.userId
-        console.log(userId)
-        if(!userId){
-            return res.status(400).json({
-                status: 'Failed',
-                message : 'userId is required'
-            })
-        }
-        const tasks = await Task.find({
-            user: userId,
-            isRemoved: false
-        })
-
-        res.status(200).json({
-            status: 'Success',
-            data : {
-                task : tasks
-            }
-        })
-        console.log(tasks)
-    }catch(err){
-        res.status(500).json({
-            status: 'Failed',
-            message : err
-        })
-    }
-})
-
-router.put('/update-task/:id', async(req,res) => {
-    console.log(req.body)
-    try{
-        const task = await Task.findByIdAndUpdate(req.params.id, req.body, {new: true})
-        console.log('x-task',task)
-        res.status(200).json({
-            status: 'Success',
-            data : {
-                task : task
-            }
-        })
-    }catch(err){
-        res.status(500).json({
-            status: 'Failed',
-            message : err
-        })
-    }
-})
-
-router.delete('/delete-task/:id', async(req,res) => {
-    try{
-        const task = await Task.findByIdAndDelete(req.params.id)
-        res.status(200).json({
-            status: 'Success',
-            data : {
-                task : task
-            }
-        })
-    }catch(err){
-        res.status(500).json({
-            status: 'Failed',
-            message : err
-        })
-    }
-})
+router.post('/add-task', TaskController.addTask)
+router.get('/get-tasks', TaskController.getTasks)
+router.put('/update-task/:id', TaskController.updateTask)
+router.delete('/delete-task/:id', TaskController.deleteTask)
 
 export default router
-
-
-
-
